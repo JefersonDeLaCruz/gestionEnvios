cc9d91bf61df09e2f89416eb73bb9c2d99f63b43
manuelmv15 <mv23036@ues.edu.sv>
2025-11-27 09:39:48 -0600

    feat: agregar roles y users

--- project/gestionEnvios/database/factories/UserFactory.php
+++ project/gestionEnvios/database/factories/UserFactory.php
@@ -24,7 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nombre' => fake()->name(),
+            'apellido' => fake()->lastName(),
+            'telefono' => fake()->phoneNumber(),
+            'direccion' => fake()->address(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
