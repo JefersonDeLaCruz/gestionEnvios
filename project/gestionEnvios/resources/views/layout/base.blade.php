73c491c861218d6ca414bb7d83ffa09d2fd714f6
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-11-27 09:37:30 -0600

    new: layout base

---
+++ project/gestionEnvios/resources/views/layout/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="synthwave">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> @yield('title', 'Mi App')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <header>
+        @yield('navbar')
+    </header>
+    
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        @yield('footer')
+    </footer>
+</body>
+</html>
---
+++ project/gestionEnvios/resources/views/layout/skeleton.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.base')
+
+@section('title', 'Home')
+@section('navbar')
+    @include('layout.navbar')
+@endsection
+@section('content')
+    <!-- usar este sample para copiar y pegar al momento de crear una nueva vista -->
+@endsection 
+@section('footer')
+    @include('layout.footer')
