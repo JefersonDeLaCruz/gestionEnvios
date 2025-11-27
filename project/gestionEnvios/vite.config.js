4e90fd2c082a1a309de48736951c3f7070c92ae0
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-11-26 21:33:29 -0600

    update: livewire & style plugins

--- project/gestionEnvios/vite.config.js
+++ project/gestionEnvios/vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',     // <--- Permite conexiones externas (Docker)
+        port: 5173,          // <--- Fijamos el puerto
+        strictPort: true,    // <--- Evita puertos aleatorios
+        hmr: {
+            host: 'localhost',   // <--- Importante para el navegador
+        },
+    },
+    
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
