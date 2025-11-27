4f6e29a5f727d6a5638f5959ecaa72f0ca516b72
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-11-27 16:10:56 -0600

    update: remove google button

--- project/gestionEnvios/resources/views/login.blade.php
+++ project/gestionEnvios/resources/views/login.blade.php
@@ -142,20 +142,8 @@
 
                 <div class="w-full max-w-[300px] flex flex-col gap-3 opacity-0 animate-enter delay-200">
 
-                    <button
-                        class="btn bg-white text-black hover:bg-gray-200 border-none rounded-full capitalize text-base font-normal flex items-center gap-2 transition-transform active:scale-95">
-                        <svg viewBox="0 0 24 24" class="h-5 w-5">
-                            <g>
-                                <path
-                                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-                            </g>
-                        </svg>
-                        Registrarse con Google
-                    </button>
 
                     <div class="flex items-center w-full my-1">
-                        <div class="h-px bg-gray-700 flex-1"></div>
-                        <span class="px-3 text-sm text-gray-200">o</span>
                         <div class="h-px bg-gray-700 flex-1"></div>
                     </div>
 
