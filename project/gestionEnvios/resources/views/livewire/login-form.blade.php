fda1a91c578e2e109f526581f3bf29e435a41c6a
kristiankovic <v23011@ues.edu.sv>
2025-11-29 14:57:59 -0600

    feat: implementacion de la funcionalidad de ver/ocultar la contraseña en el form de iniciar sesion

--- project/gestionEnvios/resources/views/livewire/login-form.blade.php
+++ project/gestionEnvios/resources/views/livewire/login-form.blade.php
@@ -36,11 +36,26 @@
                     @error('email') <span class="text-red-500 text-xs mt-1">{{ $message }}</span> @enderror
                 </div>
 
-                <div class="relative group">
-                    <input type="password" name="password" id="password" 
-                        class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-base-content bg-transparent rounded-md border border-base-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer" 
+                {{-- FUNCIONALIDAD PARA MOSTRAR LA CONTRASEÑA --}}
+                <div class="relative group" x-data="{ showPassword: false }">
+                    <input :type="showPassword ? 'text' : 'password'" name="password" id="password" 
+                        class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-base-content bg-transparent rounded-md border border-base-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer pr-10" 
                         placeholder=" " required />
+                    
                     <label for="password" class="absolute text-sm text-base-content/60 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-base-100 px-2 peer-focus:px-2 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Contraseña</label>
+                    
+                    <button type="button" 
+                            @click="showPassword = !showPassword" 
+                            class="absolute right-3 top-1/2 transform -translate-y-1/2 text-base-content/60 hover:text-base-content focus:outline-none z-20">
+                        <svg x-show="!showPassword" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <svg x-show="showPassword" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="display: none;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+                    </button>
+
                     @error('password') <span class="text-red-500 text-xs mt-1">{{ $message }}</span> @enderror
                 </div>
 
